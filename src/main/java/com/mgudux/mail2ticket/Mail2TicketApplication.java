11d94ec0b30092cb751b406fe52bcf83c2daa8fa
muhammetguducu <muhammetguducu@outlook.de>
2026-04-09 06:25:39 +0200

    Initial commit: Project-Setup

---
+++ src/main/java/com/mgudux/mail2ticket/Mail2TicketApplication.java
@@ -0,0 +1,13 @@
+package com.mgudux.mail2ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mail2TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mail2TicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgudux/mail2ticket/Mail2TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgudux.mail2ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mail2TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
